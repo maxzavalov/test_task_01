081332b5c0ae1e2b0bdc8cc29a2bf1517e59362f
Fess <maxzavalov@gmail.com>
2023-10-21 20:03:19 +0400

    add_test

--- parse_matrix.py
+++ parse_matrix.py
@@ -13,17 +13,24 @@ async def get_text(url: str) -> str:
 def text_to_array(text: str) -> np.array:
     filter_nums = re.findall(r'\d+', text)
     nums_list = [int(num) for num in filter_nums]
-    size = len(nums_list) ** 0.5
+    size = int(len(nums_list) ** 0.5)
     return np.array(nums_list).reshape(size, size)
 
 
-url = 'https://raw.githubusercontent.com/avito-tech/python-trainee-assignment/main/matrix.txt'
+def matrix_to_list(matrix: np.array) -> list[int]:
+    result = []
+    while matrix.size > 0:
+        matrix = np.rot90(matrix, -1)
+        result.extend(matrix[0][::-1])
+        matrix = np.delete(matrix, 0, axis=0)
+    return result
 
 
-async def test_get_text(url):
+async def get_matrix(url: str) -> list[int]:
     text = await get_text(url)
-    print(text)
-    return text
+    array = text_to_array(text)
+    return matrix_to_list(array)
 
 
-asyncio.run(test_get_text(url))
+if __name__ == "__main__":
+    asyncio.run(get_matrix(url))
