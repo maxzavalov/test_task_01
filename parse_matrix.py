3d0814f5d50b8e15f3f3dca27bc3d09419d1d661
Fess <maxzavalov@gmail.com>
2023-10-21 17:15:52 +0400

    reformat_text_to_array

--- parse_matrix.py
+++ parse_matrix.py
@@ -1,11 +1,20 @@
 import asyncio
 import aiohttp
+import numpy as np
+import re
 
 
 async def get_text(url: str) -> str:
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
             return await response.text()
+
+
+def text_to_array(text: str) -> np.array:
+    filter_nums = re.findall(r'\d+', text)
+    nums_list = [int(num) for num in filter_nums]
+    size = len(nums_list) ** 0.5
+    return np.array(nums_list).reshape(size, size)
 
 
 url = 'https://raw.githubusercontent.com/avito-tech/python-trainee-assignment/main/matrix.txt'
